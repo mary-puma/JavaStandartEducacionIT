6283cae472b3a5b411da44b89755c5388e61f6dc
mary-puma <pumaisabel029@gmail.com>
2022-01-19 00:30:25 -0300

    clase3

--- Clase2IntroPOO/src/main/java/com/educacionit/AppPrincipal.java
+++ Clase2IntroPOO/src/main/java/com/educacionit/AppPrincipal.java
@@ -4,48 +4,54 @@ public class AppPrincipal {
     public static void main(String[] args) {
         //instanciamos 3 objetos de la clase Auto
 
-        Auto a1 = new Auto();
-        //modificamos el estado de los atributos del objeto a1
-
-        System.out.println("-----------------objeto a1 : estado inicial de sus atributos ---------");
-        System.out.println("color : " + a1.color);
-        System.out.println("cantidadPuertas : " + a1.cantidadPuertas);
-        System.out.println("marca : " + a1.marca);
-        System.out.println("modelo : " + a1.modelo);
-        System.out.println("anio :" + a1.anio);
-        System.out.println("cilindrada :" + a1.cilindrada);
-        System.out.println("precio :" + a1.precio);
-
-        // modificamos el estado de los atributos del objeto a1
-        a1.color = "rojo";
-        a1.cantidadPuertas = 3;
-        a1.marca = "Alfa Romeo";
-        a1.modelo = "Giuletta";
-        a1.anio = 2019;
-        a1.cilindrada = 3.0f;
-        a1.precio = 15000;
-
+        Auto a1 = new Auto("rojo", 3, "Alfa Romeo", "Giuletta", 2019, 3.0f, 15000f);// se invoca al metodo contructor de
         System.out.println("-----------------objeto a1 : estado de sus atributos ---------");
-        System.out.println("color : " + a1.color);
-        System.out.println("cantidadPuertas : " + a1.cantidadPuertas);
-        System.out.println("marca : " + a1.marca);
-        System.out.println("modelo : " + a1.modelo);
-        System.out.println("anio :" + a1.anio);
-        System.out.println("cilindrada :" + a1.cilindrada);
-        System.out.println("precio :" + a1.precio);
+        System.out.println(a1.toString());
 
-        Auto a2 = new Auto();
-        System.out.println("-----------------objeto a2 : estado inicial de sus atributos ---------");
-        System.out.println("color : " + a2.color);
-        System.out.println("cantidadPuertas : " + a2.cantidadPuertas);
-        System.out.println("marca : " + a2.marca);
-        System.out.println("modelo : " + a2.modelo);
-        System.out.println("anio :" + a2.anio);
-        System.out.println("cilindrada :" + a2.cilindrada);
-        System.out.println("precio :" + a2.precio);
+        Auto a2 = new Auto("negro", 5, "Audi", "a6", 2018, 2.0f, 13000f);
+        System.out.println("-----------------objeto a2 : estado de sus atributos ---------");
+        System.out.println(a2.toString());
 
-        Auto a3 = new Auto();
+        Auto a3 = new Auto("plata", 5, "Jeep", "Grand cherokee", 2013, 6.4f, 26000f);
+        System.out.println("-----------------objeto a3 : estado de sus atributos ---------");
+        System.out.println(a3.toString());
 
-        //tarea : modificar el estados de los atributos de a2 y a3 siguiendo el Diagrama de clases
+        // clase 3 :utilizo los metodos en cada uno de los objetos
+
+        System.out.println("***************** invoco a los metodos de la clase Auto con el objeto a1");
+        a1.capacidadMaxCombutible = 50;
+        a1.arrancar(" la llave");
+        a1.acelerar(125);
+        a1.frenar();
+        a1.cargarCombustible(30);
+        a1.cargarCombustible(30);
+
+        System.out.println("***************** invoco a los metodos de la clase Auto con el objeto a2");
+        a2.capacidadMaxCombutible = 70;
+        a2.arrancar(" la tarjeta");
+        a2.acelerar(175);
+        a2.frenar();
+        a2.cargarCombustible(30);
+        a2.cargarCombustible(30);
+
+        System.out.println("***************** invoco a los metodos de la clase Auto con el objeto a3");
+        a3.capacidadActualCombutible = 5;
+        a3.capacidadMaxCombutible = 100;
+        a3.arrancar(" el boton");
+        a3.acelerar(210);
+        a3.frenar();
+        a3.cargarCombustible(85);
+
+        System.out.println("*****************  veamos los estados de los atributos ");
+        System.out.println(a1.toString());
+        System.out.println(a2.toString());
+        System.out.println(a3.toString());
+
+        Auto a4 = new Auto("Chrysler", "300C");
+        System.out.println(a4.toString());
+
     }
+
+
+
 }
--- Clase2IntroPOO/src/main/java/com/educacionit/Auto.java
+++ Clase2IntroPOO/src/main/java/com/educacionit/Auto.java
@@ -9,6 +9,90 @@ public class Auto {
     int anio;
     float cilindrada;
     float precio;
+    float capacidadMaxCombutible;
+    float capacidadActualCombutible;
 
-    //metod
+    //sobrecarga de metodos constructores
+
+    // public : modificador de acceso ,
+    public Auto() {
+        // constructor vacio no recibe ningun parametro , si es el unico constructor que bvoy a utilizar no hace
+        // falta definirlo , exite implicitamente en todas las clases
+    }
+
+    // metodo constructor con parametros
+    public Auto(String color, int cantidadPuertas, String marca, String modelo, int anio, float cilindrada,
+                float precio) {
+        this.color = color;
+        this.cantidadPuertas = cantidadPuertas;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+        this.cilindrada = cilindrada;
+        this.precio = precio;
+    }
+
+    // metodo constructor con todos los parametros
+    public Auto(String color, int cantidadPuertas, String marca, String modelo, int anio, float cilindrada,
+                float precio, float capacidadMaxCombutible, float capacidadActualCombutible) {
+        this.color = color;
+        this.cantidadPuertas = cantidadPuertas;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+        this.cilindrada = cilindrada;
+        this.precio = precio;
+        this.capacidadMaxCombutible = capacidadMaxCombutible;
+        this.capacidadActualCombutible = capacidadActualCombutible;
+    }
+
+
+    public Auto(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    //sobrecarga de metodos
+    // metodos : definen el comportamiento de los objetos
+    void acelerar(int kmXhs) {
+        System.out.println("Estoy acelerando mi " + marca + " " + modelo + " a " + kmXhs + "kmXhs");
+    }
+
+    void acelerar() {
+        System.out.println("Estoy acelerando mi " + marca + " " + modelo );
+    }
+
+    void frenar() {
+        System.out.println("Estoy frenando mi " + marca + " " + modelo);
+    }
+
+    void cargarCombustible(float litrosAcargar) {
+        if ((capacidadActualCombutible + litrosAcargar) <= capacidadMaxCombutible) {
+            capacidadActualCombutible += litrosAcargar;
+            System.out.println("Se cargo " + litrosAcargar + " litros de combustible");
+            System.out.println("Capacidad actual de su tanque " + capacidadActualCombutible + " litros");
+        } else {
+            System.out.println("No se puede cargar " + litrosAcargar + " por que su tanque no posee tanta capacidad");
+
+        }
+    }
+
+    void arrancar(String metodoEncendido) {
+        System.out.println("Estoy encendiendo con " + metodoEncendido);
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "color='" + color + '\'' +
+                ", cantidadPuertas=" + cantidadPuertas +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", anio=" + anio +
+                ", cilindrada=" + cilindrada +
+                ", precio=" + precio +
+                ", capacidadMaxCombutible=" + capacidadMaxCombutible +
+                ", capacidadActualCombutible=" + capacidadActualCombutible +
+                '}';
+    }
 }
